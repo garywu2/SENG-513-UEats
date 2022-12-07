6514dc55177c91033f8944f260009e413deba2c9
GURMUKH SINGH <dalla19956@gmail.com>
2022-12-07 13:54:05 -0700

    moved config to env file

---
+++ src/config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_UEATS_API_KEY,
+  authDomain: process.env.REACT_APP_UEATS_AUTH_DOM,
+  projectId: process.env.REACT_APP_UEATS_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_UEATS_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_UEATS_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_UEATS_APP_ID,
+};
--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "../config";
 
+console.log(firebaseConfig);
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
 export { auth };
